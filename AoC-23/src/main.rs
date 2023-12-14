08f58b5ece485b0ca66c9ad71538252ae61f2565
Patrik Michal Vlcek <patrik.vlcek@ysoft.com>
2023-12-14 09:22:13 +0100

    feat: fix main.rs

--- AoC-23/src/main.rs
+++ AoC-23/src/main.rs
@@ -2,9 +2,11 @@ pub mod day01;
 pub mod day02;
 pub mod puzzle;
 pub mod utils;
+pub mod day03;
 
 use day01::Day01;
 use day02::Day02;
+use day03::Day03;
 use puzzle::Puzzle;
 
 use crate::utils::read_input;
@@ -15,4 +17,7 @@ fn main() {
 
     println!("{}", Day02::first_star(read_input(2)));
     println!("{}", Day02::second_star(read_input(2)));
+
+    println!("{}", Day03::first_star(read_input(3)));
+    println!("{}", Day03::second_star(read_input(3)));
 }
